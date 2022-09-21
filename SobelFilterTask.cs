90f965ee76cf8d02955154c874a11bdc8bb5b050
Семена <ifkfvjif@gmail.com>
2022-09-21 17:25:02 +0500

    Реализация фильтра Собеля для выделения границы объектов, что бы они стали белыми

--- SobelFilterTask.cs
+++ SobelFilterTask.cs
@@ -4,22 +4,46 @@ namespace Recognizer
 {
     internal static class SobelFilterTask
     {
+        public static double[,] TransposeMatrix(double[,] sx)
+        {
+            // получаем размеры матрицы
+            var width = sx.GetLength(0);
+            var height = sx.GetLength(1);
+            //вводим матрицу, которая должна быть результатом транспонирования
+            var sy = new double[height, width];
+            for (var i = 0; i < width; i++)
+                for (var j = 0; j < height; j++)
+                    sy[j, i] = sx[i, j];
+            return sy;
+        }
+
+        public static double Multiply(double[,] g, double[,] xy, int shift, int x, int y)
+        {
+            var sxWidth = xy.GetLength(0);
+            var multiplication = 0.0;
+            for (var i = 0; i < sxWidth; i++)
+                for (var j = 0; j < sxWidth; j++)
+                    multiplication += xy[i, j] * g[x - shift + i, y - shift + j];
+
+            return multiplication;
+        }
+
         public static double[,] SobelFilter(double[,] g, double[,] sx)
         {
+            var result = new double[g.GetLength(0), g.GetLength(1)];
+
+            var shift = sx.GetLength(0) / 2;
+            // минимальное смещение от границ матрицы-изображения (на половину матрицы-маски)
             var width = g.GetLength(0);
             var height = g.GetLength(1);
-            var result = new double[width, height];
-            for (int x = 1; x < width - 1; x++)
-                for (int y = 1; y < height - 1; y++)
+            var sy = TransposeMatrix(sx);
+
+
+            for (var x = shift; x < width - shift; x++)
+                for (var y = shift; y < height - shift; y++) // для каждого пикселя в изображении
                 {
-                    // Вместо этого кода должно быть поэлементное умножение матриц sx и полученной транспонированием из неё sy на окрестность точки (x, y)
-                    // Такая операция ещё называется свёрткой (Сonvolution)
-                    var gx = 
-                        -g[x - 1, y - 1] - 2 * g[x, y - 1] - g[x + 1, y - 1] 
-                        + g[x - 1, y + 1] + 2 * g[x, y + 1] + g[x + 1, y + 1];
-                    var gy = 
-                        -g[x - 1, y - 1] - 2 * g[x - 1, y] - g[x - 1, y + 1] 
-                        + g[x + 1, y - 1] + 2 * g[x + 1, y] + g[x + 1, y + 1];
+                    var gx = Multiply(g, sx, shift, x, y);
+                    var gy = Multiply(g, sy, shift, x, y);
                     result[x, y] = Math.Sqrt(gx * gx + gy * gy);
                 }
             return result;
